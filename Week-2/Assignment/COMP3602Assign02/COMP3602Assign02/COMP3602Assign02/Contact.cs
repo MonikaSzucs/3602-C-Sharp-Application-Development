30c31d1841fc4592f9dae5dd4241793f6d0ef113
Monika Szucs <monika.szucs.work@gmail.com>
2023-01-15 20:38:55 -0800

    Finished looping through and creating tabbed data - still need to organize code though

---
+++ Week-2/Assignment/COMP3602Assign02/COMP3602Assign02/COMP3602Assign02/Contact.cs
@@ -0,0 +1,70 @@
+﻿// Monika Szucs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3602Assign02
+{
+    class Contact
+    {   
+        // fields
+        private string firstName;
+        private string lastName;
+        private string age;
+        private string address;
+        private string city;
+        private string province;
+        private string postalCode;
+
+        // ctor
+        public Contact(string firstName, string lastName, string age, string address, string city, string province, string postalCode)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.address = address;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode;
+        }
+        // readonly Property Method
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string Province
+        {
+            get { return province; }
+            set { province = value; }
+        }
+        public string PostalCode
+        {
+            get { return postalCode; }
+            set { postalCode = value; }
+        }
+    }
+}
---
+++ Week-2/Assignment/COMP3602Assign02/COMP3602Assign02/COMP3602Assign02/Program.cs
@@ -0,0 +1,53 @@
+﻿// Monika Szucs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3602Assign02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "COMP3602 - Assignment 2 - A00878763";
+
+            Console.Write("Enter Contact Information: \n");
+
+            Contact[] people = new Contact[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(new string('-', 30));
+                Console.Write("First Name:\t");
+                string firstName = Console.ReadLine();
+                Console.Write("Last Name:\t");
+                string lastName = Console.ReadLine();
+                Console.Write("Age:\t\t");
+                string age = Console.ReadLine();
+                Console.Write("Address:\t");
+                string address = Console.ReadLine();
+                Console.Write("City:\t\t");
+                string city = Console.ReadLine();
+                Console.Write("Province:\t");
+                string province = Console.ReadLine();
+                Console.Write("Postal Code:\t");
+                string postalCode = Console.ReadLine();
+
+                people[i] = new Contact(firstName, lastName, age, address, city, province, postalCode);
+            }
+            Console.WriteLine(new string('-', 30));
+
+            Console.Write("\nContacts Entered: \n");
+            Console.WriteLine(new string('-', 30));
+
+            foreach (Contact person in people)
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName} ({person.Age})\n" +
+                                    $"{person.Address}\n" +
+                                    $"{person.City} {person.Province}  {person.PostalCode}\n\n");
+            }
+        }
+    }
+}
