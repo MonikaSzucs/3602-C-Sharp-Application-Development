aefaf15c3e3a45902d8aca57a3f2fc272d1eb7fa
Monika Szucs <monika.szucs.work@gmail.com>
2023-02-16 10:30:23 -0800

    started lab 6

---
+++ Week-6/Lab/COMP3602Lab06/COMP3602Lab06/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3602Lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "COMP3602 Lab06 A00878763";
+        }
+    }
+}
