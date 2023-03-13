17c8774e34ea10d9182d6efdb79ac0f557b728f3
Monika Szucs <monika_szucs@bcit.ca>
2023-03-12 21:09:30 -0700

    working on assignment

---
+++ Week-9/Assignment/Assignment Version 6/Assign07/Assign07/ClientRepository.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign07
+{
+    class ClientRepository
+    {
+        private static readonly string connString = @"Server=tcp:BUNKER3.EDU.BCIT.CA,1433;
+                                                    Initial Catalog=TigerDB;
+                                                    User ID=tiger; 
+                                                    Password=T1gerM@ster;
+                                                    Encrypt=True;
+                                                    TrustServerCertificate=True; 
+                                                    Connection Timeout=30;";
+
+        public static ClientList GetClients(decimal gstRate, decimal pstRate)
+        {
+            ClientList clients = new ClientList();
+            int clientId = 1;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    string query = @"SELECT CompanyName, Address1, Address2, City, Province, PostalCode, YTDSales, CreditHold, Notes, IsTaxable
+                                    FROM Client01";
+
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandText = query;
+                        cmd.Connection = conn;
+
+                        conn.Open();
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string companyName = reader["CompanyName"] as string;
+                                string address1 = reader["Address1"] as string;
+                                string? address2 = reader["Address2"] as string;
+                                string? city = reader["City"] as string;
+                                string province = reader["Province"] as string;
+                                string? postalCode = reader["PostalCode"] as string;
+                                decimal ytdSales = (decimal)reader["YTDSales"];
+
+                                bool? creditHold = reader["CreditHold"] as bool?;
+                                bool isCreditHold = false;
+                                if (creditHold == true)
+                                {
+                                    isCreditHold = true;
+                                }
+                                else if (creditHold == false)
+                                {
+                                    isCreditHold = false;
+                                }
+
+                                string notes = reader["Notes"] as string;
+
+                                bool? taxable = reader["IsTaxable"] as bool?;
+                                bool isTaxable = false;
+                                if (taxable == true)
+                                {
+                                    isTaxable = true;
+                                }
+                                else if (taxable == false)
+                                {
+                                    isTaxable = false;
+                                }
+
+                                
+
+
+                                if(isTaxable == false)
+                                {
+                                    clients.Add(new Client(0.0m, 0.0m) { ClientId = clientId, CompanyName = companyName, Address1 = address1, Address2 = address2, City = city, Province = province, PostalCode = postalCode, YTDSales = ytdSales, CreditHold = isCreditHold, Notes = notes, IsTaxable = isTaxable });
+                                    clientId++;
+                                } else
+                                {
+                                    clients.Add(new Client(gstRate, pstRate) { ClientId = clientId, CompanyName = companyName, Address1 = address1, Address2 = address2, City = city, Province = province, PostalCode = postalCode, YTDSales = ytdSales, CreditHold = isCreditHold, Notes = notes, IsTaxable = isTaxable });
+                                    clientId++;
+                                }
+
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return clients;
+        }
+    }
+}
--- Week-9/In-Class/In Class/ModalDialogDemo/ModalDialogDemo/EditDialog.Designer.cs
+++ Week-9/In-Class/In Class/ModalDialogDemo/ModalDialogDemo/EditDialog.Designer.cs
@@ -38,17 +38,19 @@
             // 
             this.textBoxUserName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxUserName.Location = new System.Drawing.Point(81, 20);
+            this.textBoxUserName.Location = new System.Drawing.Point(162, 38);
+            this.textBoxUserName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBoxUserName.Name = "textBoxUserName";
-            this.textBoxUserName.Size = new System.Drawing.Size(282, 20);
+            this.textBoxUserName.Size = new System.Drawing.Size(560, 31);
             this.textBoxUserName.TabIndex = 5;
             // 
             // labelUserNamePrompt
             // 
             this.labelUserNamePrompt.AutoSize = true;
-            this.labelUserNamePrompt.Location = new System.Drawing.Point(12, 24);
+            this.labelUserNamePrompt.Location = new System.Drawing.Point(24, 46);
+            this.labelUserNamePrompt.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelUserNamePrompt.Name = "labelUserNamePrompt";
-            this.labelUserNamePrompt.Size = new System.Drawing.Size(63, 13);
+            this.labelUserNamePrompt.Size = new System.Drawing.Size(125, 25);
             this.labelUserNamePrompt.TabIndex = 4;
             this.labelUserNamePrompt.Text = "&User Name:";
             // 
@@ -56,9 +58,10 @@
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(286, 67);
+            this.buttonCancel.Location = new System.Drawing.Point(572, 129);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(150, 44);
             this.buttonCancel.TabIndex = 7;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -66,9 +69,10 @@
             // buttonOk
             // 
             this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonOk.Location = new System.Drawing.Point(192, 67);
+            this.buttonOk.Location = new System.Drawing.Point(384, 129);
+            this.buttonOk.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonOk.Name = "buttonOk";
-            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.Size = new System.Drawing.Size(150, 44);
             this.buttonOk.TabIndex = 6;
             this.buttonOk.Text = "Ok";
             this.buttonOk.UseVisualStyleBackColor = true;
@@ -77,16 +81,16 @@
             // EditDialog
             // 
             this.AcceptButton = this.buttonOk;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(383, 107);
+            this.ClientSize = new System.Drawing.Size(766, 206);
             this.Controls.Add(this.textBoxUserName);
             this.Controls.Add(this.labelUserNamePrompt);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOk);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "EditDialog";
